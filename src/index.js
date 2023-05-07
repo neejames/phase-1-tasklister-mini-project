291f43d78668d6357143bb07a8f126f3f139227c
Neechelle James <neechelle.james@gmail.com>
2023-05-07 15:39:33 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+ document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault()
+  addTask(e.target["new-task-description"].value)
+ })
 });
+
+function addTask(toDo) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.textContent = 'x'
+  btn.addEventListener('click', handleDelte)
+  p.textContent = `${toDo}`
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#tasks').appendChild(p)
+}
+
+function handleDelte(e) {
+  e.target.parentNode.remove()
+}
